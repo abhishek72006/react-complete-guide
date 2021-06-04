e64d44a2cc9a32b988cb5da95d02a1f3537b68d2
abhishek <abhishekwagle2015@gmail.com>
2021-06-04 08:54:28 +0545

    adding data dynamically

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App(){
   return (
       <div>
       <ExpenseItem/>
-      <img className="apple" src="https://www.applesfromny.com/wp-content/uploads/2020/05/Jonagold_NYAS-Apples2.png"/>
+      <img className="apple" src="https://www.applesfromny.com/wp-content/uploads/2020/05/Jonagold_NYAS-Apples2.png"/> 
       </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./ExpenseItem.css";
 function ExpenseItem() {
+  const expneseDate = new Date(2021, 5,  3);
+  const expenseItem = "Car Insurance";
+  const expenseAmount = 294.67;
   return (
     <div className="expense-item">
-      <div>March 28th 2021</div>
+      <div>{expneseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.67</div>
+        <h2>{expenseItem}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
